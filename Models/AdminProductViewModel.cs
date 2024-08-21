2f3256d6c0d732e5cf8d2c4d947312714a647076
tonhat211 <21130463@st.hcmuaf.edu.vn>
2024-08-22 00:40:42 +0700

    hoan thanh admin san pham

---
+++ Models/AdminProductViewModel.cs
@@ -0,0 +1,48 @@
+﻿using PhuKienShop.Data;
+
+namespace PhuKienShop.Models
+{
+    public class AdminProductViewModel
+    {
+        public Product product { get; set; }
+        public Category category { get; set; }
+
+        public List<Product> products { get; set; }
+        public List<Category> categories { get; set; }
+
+        
+        public AdminProductViewModel(Product _product)
+        {
+            product = _product;
+        }
+        public AdminProductViewModel(Product _product, List<Category> _categories)
+        {
+            product = _product;
+            categories = _categories;
+        }
+
+       
+        public AdminProductViewModel(List<Product> _products, List<Category> _categories)
+        {
+            products = _products;
+            categories = _categories;
+        } 
+        
+        public AdminProductViewModel(List<Product> _products)
+        {
+            products = _products;
+          
+        }
+
+        public AdminProductViewModel(Category _category)
+        {
+            category = _category;
+        }
+        public AdminProductViewModel()
+        {
+          
+        }
+    }
+
+ 
+}
---
+++ Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PhuKienShop.Data; // Đảm bảo bạn sử dụng namespace chính xác
+
+namespace PhuKienShop.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> SelectAllAsync();
+    }
+}
