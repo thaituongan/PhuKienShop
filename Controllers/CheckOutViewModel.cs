8f14967d00586df973650f9b116dece95ba9031c
Nguyen-Thi-Thanh-Ngan <21130116@st.hcmuaf.edu.vn>
2024-08-21 11:18:21 +0700

    add check out

--- Controllers/CheckOutViewModel.cs
+++ Controllers/CheckOutViewModel.cs
@@ -3,26 +3,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PhuKienShop.Models
 {
-	public class CheckoutViewModel
-	{
-		[Required]
-		public int UserId { get; set; }
+    public class CheckoutViewModel
+    {
+        [Required]
+        public int UserId { get; set; }
 
-		[Required]
-		public decimal TotalAmount { get; set; }
+        [Required]
+        public decimal TotalAmount { get; set; }
 
-		public List<OrderDetailViewModel> OrderDetails { get; set; } = new List<OrderDetailViewModel>();
+        public List<OrderDetailViewModel> OrderDetails { get; set; } = new List<OrderDetailViewModel>();
 
-		public class OrderDetailViewModel
-		{
-			[Required]
-			public int ProductId { get; set; }
 
-			[Required]
-			public int Quantity { get; set; }
+        public class OrderDetailViewModel
+        {
+            [Required]
+            public int ProductId { get; set; }
 
-			[Required]
-			public decimal UnitPrice { get; set; }
-		}
-	}
+            [Required]
+            public int Quantity { get; set; }
+
+            [Required]
+            public decimal UnitPrice { get; set; }
+        }
+
+        public List<CartProduct> CartProducts { get; set; } = new List<CartProduct>();
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string OrderNotes { get; set; }
+    }
 }
